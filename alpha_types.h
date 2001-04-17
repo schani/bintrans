e41e59e58cd31f09e2714d60631c1d8fa295c22a
schani <schani@cefd48d6-89e4-0310-8b86-be731dd2b999>
2001-04-17 01:01:39 +0000

    Missing alpha_types.h included.
    git-svn-id: http://localhost/svn/unix/bintrans/trunk@14 cefd48d6-89e4-0310-8b86-be731dd2b999

---
+++ alpha_types.h
@@ -0,0 +1,9 @@
+typedef unsigned int word_32;
+typedef unsigned short word_16;
+typedef unsigned long word_64;
+typedef unsigned char word_8;
+typedef word_8 byte;
+typedef signed int sword_32;
+typedef signed long sword_64;
+
+typedef unsigned long addr_t;
